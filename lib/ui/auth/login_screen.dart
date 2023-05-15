0d7726d89ba83f5c0f95074a87ce75431bb9b233
Harshal Kothari <190540107113@darshan.ac.in>
2023-05-15 18:08:01 +0530

    signup_screen

--- lib/ui/auth/login_screen.dart
+++ lib/ui/auth/login_screen.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_app/ui/auth/signup_screen.dart';
 import 'package:firebase_app/widgets/round_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -23,74 +25,86 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        centerTitle: true,
-        title: Text("Login"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Form(
-              key: _formKey,
-              child: Column(
+    return WillPopScope(
+      onWillPop: () async {
+        SystemNavigator.pop();
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          title: Text("Login"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      controller: emailControllor,
+                      decoration: const InputDecoration(
+                        hintText: 'Email',
+                        suffixIcon: Icon(Icons.email_outlined),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Enter an email';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      controller: passwordControllor,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        hintText: 'Password',
+                        suffixIcon: Icon(Icons.lock),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Enter a password';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 40),
+                  ],
+                ),
+              ),
+              RoundButton(
+                title: "Login",
+                onTap: () {
+                  if (_formKey.currentState!.validate()) {}
+                },
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  TextFormField(
-                    keyboardType: TextInputType.emailAddress,
-                    controller: emailControllor,
-                    decoration: const InputDecoration(
-                      hintText: 'Email',
-                      suffixIcon: Icon(Icons.email_outlined),
-                    ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        print("--------");
-                        return 'Enter an email';
-                      }
-                      return null;
+                  Text("Don't have an account?"),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (_) {
+                          return SignUpScreen();
+                        }),
+                      );
                     },
+                    child: Text("Sign Up"),
                   ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    controller: passwordControllor,
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      hintText: 'Password',
-                      suffixIcon: Icon(Icons.lock),
-                    ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Enter a password';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 40),
                 ],
               ),
-            ),
-            RoundButton(
-              title: "Login",
-              onTap: () {
-                if (_formKey.currentState!.validate()) {}
-              },
-            ),
-            const SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Don't have an account?"),
-                TextButton(
-                  onPressed: () {},
-                  child: Text("Sign Up"),
-                ),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
